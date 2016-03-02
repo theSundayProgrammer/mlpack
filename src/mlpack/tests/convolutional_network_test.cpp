9bcb040b2ab3c108771a076f05fc5bfcf638b53b
theSundayProgrammer <joe.mariadassou@gmail.com>
2016-03-03 05:24:33 +1100

    adding a unit test for copy construction

--- src/mlpack/tests/convolutional_network_test.cpp
+++ src/mlpack/tests/convolutional_network_test.cpp
@@ -1,9 +1,9 @@
 /**
- * @file convolutional_network_test.cpp
- * @author Marcus Edel
- *
- * Tests the convolutional neural network.
- */
+* @file convolutional_network_test.cpp
+* @author Marcus Edel
+*
+* Tests the convolutional neural network.
+*/
 #include <mlpack/core.hpp>
 
 #include <mlpack/methods/ann/activation_functions/logistic_function.hpp>
@@ -32,12 +32,7 @@ using namespace mlpack::optimization;
 
 BOOST_AUTO_TEST_SUITE(ConvolutionalNetworkTest);
 
-/**
- * Train and evaluate a vanilla network with the specified structure.
- */
-template<
-    typename PerformanceFunction
->
+
 void BuildVanillaNetwork()
 {
   arma::mat X;
@@ -69,27 +64,33 @@ void BuildVanillaNetwork()
     input.slice(i) = arma::mat(X.colptr(i), 28, 28);
 
   /*
-   * Construct a convolutional neural network with a 28x28x1 input layer,
-   * 24x24x8 convolution layer, 12x12x8 pooling layer, 8x8x12 convolution layer
-   * and a 4x4x12 pooling layer which is fully connected with the output layer.
-   * The network structure looks like:
-   *
-   * Input    Convolution  Pooling      Convolution  Pooling      Output
-   * Layer    Layer        Layer        Layer        Layer        Layer
-   *
-   *          +---+        +---+        +---+        +---+
-   *          | +---+      | +---+      | +---+      | +---+
-   * +---+    | | +---+    | | +---+    | | +---+    | | +---+    +---+
-   * |   |    | | |   |    | | |   |    | | |   |    | | |   |    |   |
-   * |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> |   |
-   * |   |      +-+   |      +-+   |      +-+   |      +-+   |    |   |
-   * +---+        +---+        +---+        +---+        +---+    +---+
-   */
+  * Construct a convolutional neural network with a 28x28x1 input layer,
+  * 24x24x8 convolution layer, 12x12x8 pooling layer, 8x8x12 convolution layer
+  * and a 4x4x12 pooling layer which is fully connected with the output layer.
+  * The network structure looks like:
+  *
+  * Input    Convolution  Pooling      Convolution  Pooling      Output
+  * Layer    Layer        Layer        Layer        Layer        Layer
+  *
+  *          +---+        +---+        +---+        +---+
+  *          | +---+      | +---+      | +---+      | +---+
+  * +---+    | | +---+    | | +---+    | | +---+    | | +---+    +---+
+  * |   |    | | |   |    | | |   |    | | |   |    | | |   |    |   |
+  * |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> |   |
+  * |   |      +-+   |      +-+   |      +-+   |      +-+   |    |   |
+  * +---+        +---+        +---+        +---+        +---+    +---+
+  */
 
   ConvLayer<> convLayer0(1, 8, 5, 5);
   BiasLayer2D<> biasLayer0(8);
   BaseLayer2D<> baseLayer0;
   PoolingLayer<> poolingLayer0(2);
+
+  ConvLayer<> convLayer01(convLayer0);
+  BiasLayer2D<> biasLayer01(biasLayer0);
+  BaseLayer2D<> baseLayer01(baseLayer0);
+  PoolingLayer<> poolingLayer01(poolingLayer0);
+
 
   ConvLayer<> convLayer1(8, 12, 5, 5);
   BiasLayer2D<> biasLayer1(12);
@@ -100,16 +101,25 @@ void BuildVanillaNetwork()
   BiasLayer<> biasLayer2(10);
   SoftmaxLayer<> softmaxLayer0;
 
+  LinearMappingLayer<> linearLayer01(linearLayer0);
+  BiasLayer<> biasLayer21(biasLayer2);
+  SoftmaxLayer<> softmaxLayer01(softmaxLayer0);
+
   OneHotLayer outputLayer;
-
-  auto modules = std::tie(convLayer0, biasLayer0, baseLayer0, poolingLayer0,
-                          convLayer1, biasLayer1, baseLayer1, poolingLayer1,
-                          linearLayer0, biasLayer2, softmaxLayer0);
-
-  CNN<decltype(modules), decltype(outputLayer),
-      RandomInitialization, MeanSquaredErrorFunction> net(modules, outputLayer);
   biasLayer0.Weights().zeros();
   biasLayer1.Weights().zeros();
+
+  auto modules = std::make_tuple(convLayer01, biasLayer01, baseLayer01, poolingLayer01,
+    convLayer1, biasLayer1, baseLayer1, poolingLayer1,
+    linearLayer01, biasLayer21, softmaxLayer01);
+
+  //
+  //auto modules = std::tie(convLayer0, biasLayer0, baseLayer0, poolingLayer0,
+  //  convLayer1, biasLayer1, baseLayer1, poolingLayer1,
+  //  linearLayer0, biasLayer2, softmaxLayer0);
+
+  CNN<decltype(modules), decltype(outputLayer),
+    RandomInitialization, MeanSquaredErrorFunction> net(std::move(modules), std::move(outputLayer));
 
   RMSprop<decltype(net)> opt(net, 0.01, 0.88, 1e-8, 10 * input.n_slices, 0);
 
@@ -122,7 +132,111 @@ void BuildVanillaNetwork()
   for (size_t i = 0; i < nPoints; i++)
   {
     if (arma::sum(arma::sum(
-        arma::abs(prediction.col(i) - Y.col(i)))) == 0)
+      arma::abs(prediction.col(i) - Y.col(i)))) == 0)
+    {
+      error++;
+    }
+  }
+
+  double classificationError = 1 - double(error) / nPoints;
+  BOOST_REQUIRE_LE(classificationError, 0.6);
+}
+auto CopyVanillaNetwork()
+{
+
+
+  /*
+  * Construct a convolutional neural network with a 28x28x1 input layer,
+  * 24x24x8 convolution layer, 12x12x8 pooling layer, 8x8x12 convolution layer
+  * and a 4x4x12 pooling layer which is fully connected with the output layer.
+  * The network structure looks like:
+  *
+  * Input    Convolution  Pooling      Convolution  Pooling      Output
+  * Layer    Layer        Layer        Layer        Layer        Layer
+  *
+  *          +---+        +---+        +---+        +---+
+  *          | +---+      | +---+      | +---+      | +---+
+  * +---+    | | +---+    | | +---+    | | +---+    | | +---+    +---+
+  * |   |    | | |   |    | | |   |    | | |   |    | | |   |    |   |
+  * |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> +-+ |   +--> |   |
+  * |   |      +-+   |      +-+   |      +-+   |      +-+   |    |   |
+  * +---+        +---+        +---+        +---+        +---+    +---+
+  */
+
+  ConvLayer<> convLayer0(1, 8, 5, 5);
+  BiasLayer2D<> biasLayer0(8);
+  BaseLayer2D<> baseLayer0;
+  PoolingLayer<> poolingLayer0(2);
+
+
+
+  ConvLayer<> convLayer1(8, 12, 5, 5);
+  BiasLayer2D<> biasLayer1(12);
+  BaseLayer2D<> baseLayer1;
+  PoolingLayer<> poolingLayer1(2);
+
+  LinearMappingLayer<> linearLayer0(192, 10);
+  BiasLayer<> biasLayer2(10);
+  SoftmaxLayer<> softmaxLayer0;
+
+  OneHotLayer outputLayer;
+  biasLayer0.Weights().zeros();
+  biasLayer1.Weights().zeros();
+
+  auto modules = std::make_tuple(convLayer0, biasLayer0, baseLayer0, poolingLayer0,
+    convLayer1, biasLayer1, baseLayer1, poolingLayer1,
+    linearLayer0, biasLayer2, softmaxLayer0);
+
+  CNN<decltype(modules), decltype(outputLayer),
+    RandomInitialization, MeanSquaredErrorFunction> net(std::move(modules), std::move(outputLayer));
+  return net;
+
+}
+/**
+* Train the vanilla network on a larger dataset.
+*/
+BOOST_AUTO_TEST_CASE(CopyVanillaNetworkTest)
+{
+  arma::mat X;
+  X.load("mnist_first250_training_4s_and_9s.arm");
+
+  // Normalize each point since these are images.
+  arma::uword nPoints = X.n_cols;
+  for (arma::uword i = 0; i < nPoints; i++)
+  {
+    X.col(i) /= norm(X.col(i), 2);
+  }
+
+  // Build the target matrix.
+  arma::mat Y = arma::zeros<arma::mat>(10, nPoints);
+  for (size_t i = 0; i < nPoints; i++)
+  {
+    if (i < nPoints / 2)
+    {
+      Y.col(i)(5) = 1;
+    }
+    else
+    {
+      Y.col(i)(8) = 1;
+    }
+  }
+
+  arma::cube input = arma::cube(28, 28, nPoints);
+  for (size_t i = 0; i < nPoints; i++)
+    input.slice(i) = arma::mat(X.colptr(i), 28, 28);
+  auto net = CopyVanillaNetwork();
+  RMSprop<decltype(net)> opt(net, 0.01, 0.88, 1e-8, 10 * input.n_slices, 0);
+
+  net.Train(input, Y, opt);
+
+  arma::mat prediction;
+  net.Predict(input, prediction);
+
+  size_t error = 0;
+  for (size_t i = 0; i < nPoints; i++)
+  {
+    if (arma::sum(arma::sum(
+      arma::abs(prediction.col(i) - Y.col(i)))) == 0)
     {
       error++;
     }
@@ -133,11 +247,12 @@ void BuildVanillaNetwork()
 }
 
 /**
- * Train the vanilla network on a larger dataset.
- */
-BOOST_AUTO_TEST_CASE(VanillaNetworkTest)
+* Train the vanilla network on a larger dataset.
+*/
+BOOST_AUTO_TEST_CASE(ConvVanillaNetworkTest)
 {
-  BuildVanillaNetwork<LogisticFunction>();  
+  BuildVanillaNetwork();
 }
+
 
 BOOST_AUTO_TEST_SUITE_END();
